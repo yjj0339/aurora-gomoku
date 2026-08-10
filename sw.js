9aae470e890de5a761897625e7e0519546424ff0
yjj0339 <2030596610@qq.com>
2026-08-10 22:31:06 +0800

    deploy: sw.js

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE = 'aurora-gomoku-v1.0.0'
+const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icons/icon.svg']
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(APP_SHELL)))
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))),
+  )
+  self.clients.claim()
+})
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        if (response.ok && new URL(event.request.url).origin === location.origin) {
+          const copy = response.clone()
+          caches.open(CACHE).then((cache) => cache.put(event.request, copy))
+        }
+        return response
+      })
+      .catch(() => caches.match(event.request).then((cached) => cached || caches.match('./index.html'))),
+  )
+})
